f2e54bfdba25ad56e7710f8943bb82aaacadd8be
GabrielRjBr <gbsilva.rj.br@gmail.com>
2025-09-07 15:54:37 -0300

    \Primeiro commit do projeto"

---
+++ src/app/detalhes/detalhes.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-detalhes',
+  templateUrl: './detalhes.page.html',
+  styleUrls: ['./detalhes.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, RouterModule]
+})
+export class DetalhesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
